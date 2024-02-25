3a5584971a18d978499fbe130fa66dc40fbec721
wfjy01 <wfjy01@users.noreply.github.com>
2024-02-25 13:03:09 +0000

    最新版本

---
+++ 后台php代码/Application/Home/Conf/config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'DB_TYPE'               =>  'mysql',     // 数据库类型
+	'DB_HOST'               =>  '47.108.238.90', // 服务器地址
+	'DB_NAME'               =>  'vueapi',          // 数据库名
+	'DB_USER'               =>  'root1',      // 用户名
+	'DB_PWD'                =>  'root1!Cofco.Com',          // 密码
+	'DB_PORT'               =>  '3306',        // 端口
+);
